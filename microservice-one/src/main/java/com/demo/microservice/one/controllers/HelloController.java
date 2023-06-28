57fe9bc899f7a795d0ba63b1fd8a0809a183390b
sriram-ponangi <19464979+sriram-ponangi@users.noreply.github.com>
2023-06-28 00:24:55 -0300

    Adding Kubernetes Config YAML with ConfigMaps

--- microservice-one/src/main/java/com/demo/microservice/one/controllers/HelloController.java
+++ microservice-one/src/main/java/com/demo/microservice/one/controllers/HelloController.java
@@ -1,14 +1,18 @@
 package com.demo.microservice.one.controllers;
 
 import com.demo.microservice.one.models.HelloMessage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${demo.greeting.message:Hello World :D !}")
+    private String greeterMessage;
+
     @GetMapping("/hello")
     private HelloMessage getMessage() {
-        return new HelloMessage("Hello World");
+        return new HelloMessage(greeterMessage);
     }
 }
