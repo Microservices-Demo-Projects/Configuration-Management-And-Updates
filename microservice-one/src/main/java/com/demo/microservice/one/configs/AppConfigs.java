52f0b18cb84806e346a3b5767fc2c75bf083a4c3
sriram-ponangi <19464979+sriram-ponangi@users.noreply.github.com>
2023-06-28 23:24:24 -0300

    Adding configs with refresh

---
+++ microservice-one/src/main/java/com/demo/microservice/one/configs/AppConfigs.java
@@ -0,0 +1,15 @@
+package com.demo.microservice.one.configs;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@RefreshScope
+@Configuration
+public class AppConfigs {
+
+    @Value("${demo.greeting.message:Hello World from local!!}")
+    private String greetingMessage;
+}
--- microservice-one/src/main/java/com/demo/microservice/one/controllers/HelloController.java
+++ microservice-one/src/main/java/com/demo/microservice/one/controllers/HelloController.java
@@ -1,18 +1,20 @@
 package com.demo.microservice.one.controllers;
 
+import com.demo.microservice.one.configs.AppConfigs;
 import com.demo.microservice.one.models.HelloMessage;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final AppConfigs appConfigs;
 
-    @Value("${demo.greeting.message:Hello World :D !}")
-    private String greeterMessage;
+    public HelloController(AppConfigs appConfigs) {
+        this.appConfigs = appConfigs;
+    }
 
     @GetMapping("/hello")
     private HelloMessage getMessage() {
-        return new HelloMessage(greeterMessage);
+        return new HelloMessage(appConfigs.getGreetingMessage());
     }
 }
